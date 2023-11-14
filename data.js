79f6dba113dc6accce26405081df5dcc2fa8376f
govardytka <govardyes@gmail.com>
2023-11-14 19:19:08 +0800

    new scene

--- data.js
+++ data.js
@@ -28,9 +28,51 @@ var APP_DATA = {
       ],
       "faceSize": 4096,
       "initialViewParameters": {
-        "yaw": -0.3146019794254755,
-        "pitch": -0.06370295629776379,
+        "yaw": 0.5358860158470016,
+        "pitch": -0.12616146756286284,
         "fov": 1.0934084192399574
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7759732674500377,
+          "pitch": -0.07298432205714178,
+          "rotation": 0,
+          "target": "1-20230928_142435_211"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-20230928_142435_211",
+      "name": "20230928_142435_211",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
